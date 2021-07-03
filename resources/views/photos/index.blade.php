82e9de375b4e90fc0db728f096d01b8c89282d46
Rahmat Setiawan <setiawaneggy@gmail.com>
2021-07-03 22:21:23 +0700

    fix: card header index photos

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -9,7 +9,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header"><i class="fa fa-align-justify"></i> Daftar Kriteria</div>
+                <div class="card-header"><i class="fa fa-align-justify"></i> Semua Foto</div>
                 <div class="card-body">
                     <livewire:photos.index />
                 </div>
